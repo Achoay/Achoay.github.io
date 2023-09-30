ec6b95d7102c9a5c568f63e4df8b85cd5cd22ae1
Achoay <clg80@me.com>
2023-09-30 13:23:49 +0200

    feat:add the colour page with the colour picker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+// Save name player
+
 const saveName = () => {
 
     let valor = document.getElementById("playerName").value;
@@ -5,4 +8,24 @@ const saveName = () => {
     sessionStorage.setItem("name", valor);
 
     window.location.href = "../result.html";
+}
+
+// Colour Picker
+
+function startup() {
+    let colorPicker = document.querySelector("#colorPicker");
+    colorPicker.value = "#8a2be2";
+    colorPicker.addEventListener("input", updateFirst, false);
+    colorPicker.addEventListener("change", updateAll, false);
+    colorPicker.select();
+}
+
+function updateFirst(event) {
+    const colorSquare = document.getElementById("square1");
+    colorSquare.style.backgroundColor = event.target.value;
+}
+
+function updateAll(event) {
+    const colorSquare = document.getElementById("square1");
+    colorSquare.style.backgroundColor = event.target.value;
 }
