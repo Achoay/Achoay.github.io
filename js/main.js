f341189df09ebe29b3721c17f5b96ed214588ab5
Achoay <clg80@me.com>
2023-09-30 16:55:58 +0200

    feat:create the function to the make the colour selection

--- js/main.js
+++ js/main.js
@@ -1,31 +1,47 @@
-
 // Save name player
 
 const saveName = () => {
-
-    let valor = document.getElementById("playerName").value;
-
-    sessionStorage.setItem("name", valor);
-
-    window.location.href = "../result.html";
-}
+  let valor = document.getElementById("playerName").value;
+  sessionStorage.setItem("name", valor);
+  window.addEventListener("load", startup, false);
+};
 
 // Colour Picker
 
-function startup() {
-    let colorPicker = document.querySelector("#colorPicker");
+let colorPicker = document.getElementsByClassName("colorpicker");
+let arrayColorPicker = Array.from(colorPicker);
+function startup(event) {
+  arrayColorPicker.map((elemento) => {
     colorPicker.value = "#8a2be2";
     colorPicker.addEventListener("input", updateFirst, false);
     colorPicker.addEventListener("change", updateAll, false);
     colorPicker.select();
+  }
+  )
 }
 
 function updateFirst(event) {
-    const colorSquare = document.getElementById("square1");
-    colorSquare.style.backgroundColor = event.target.value;
+  const colorSquare = document.getElementById("square1");
+  colorSquare.style.backgroundColor = event.target.value;
 }
 
 function updateAll(event) {
-    const colorSquare = document.getElementById("square1");
-    colorSquare.style.backgroundColor = event.target.value;
+  const colorSquare = document.getElementById("square1");
+  colorSquare.style.backgroundColor = event.target.value;
 }
+
+// Hidden Rows
+
+const saveLevelBeginner = () => {
+    sessionStorage.setItem("level", "beginnerRow");
+    window.location.href = "./colores.html";
+}
+const saveLevelIntermediate = () => {
+    sessionStorage.setItem("level", "intermediateRow");
+    window.location.href = "./colores.html";
+}
+window.onload = (event) => {
+    let selectedLevel = sessionStorage.getItem("level");
+    let selected = document.getElementById(selectedLevel);
+    selected.style.display = "flex";
+};
