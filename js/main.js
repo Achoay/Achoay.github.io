801240b3a5798995dd75dfd3534bb558622b18fc
Achoay <clg80@me.com>
2023-10-01 19:07:42 +0200

    feat:create function to add rows to the game page

--- js/main.js
+++ js/main.js
@@ -17,13 +17,13 @@ function startup(event) {
     arrayColorPicker.map(
         (element) => {
             element.value = "#8a2be2";
-            element.addEventListener("input", (event)=> updateFirst(event, element), false);
+            element.addEventListener("input", (event) => updateFirst(event, element), false);
             element.select();
         }
     )
 }
 
-const updateFirst = (event,element) => {
+const updateFirst = (event, element) => {
 
   let colorSquare = document.getElementById(`square${element.id}`);
   colorSquare.style.backgroundColor = event.target.value;
@@ -51,8 +51,40 @@ const saveLevelAdvanced = () => {
     sessionStorage.setItem("level", "advancedRow");
     window.location.href = "./colours.html";
 }
-window.onload = (event) => {
     let selectedLevel = sessionStorage.getItem("level");
     let selected = document.getElementById(selectedLevel);
+    window.onload = (event) => {
     selected.style.display = "flex";
+};
+
+// Game
+
+window.addEventListener("load", ()=>createRows());
+
+let board = document.getElementById("game");
+
+const createRows = () => {
+    let mainCol = document.createElement("div");
+    mainCol.className = "col-12 d-flex";
+    let squaresDiv = document.createElement("div");
+    squaresDiv.className = "d-flex justify-content-evenly";
+
+    for(let i=0; i < 4; i++){
+        let eachSquare = document.createElement("div");
+        eachSquare.className = "squareGame m-2";
+        squaresDiv.appendChild(eachSquare)
+    }
+
+    let circlesDiv = document.createElement("div");
+    circlesDiv.className = "d-flex justify-content-evenly align-items-center";
+
+    for(let i=0; i < 4; i++){
+        let eachCircle = document.createElement("div");
+        eachCircle.className = "circle m-1";
+        circlesDiv.appendChild(eachCircle)
+    }
+
+    mainCol.appendChild(squaresDiv);
+    mainCol.appendChild(circlesDiv);
+    board.appendChild(mainCol);
 };
